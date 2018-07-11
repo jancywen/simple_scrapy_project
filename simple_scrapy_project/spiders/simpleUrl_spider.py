30b1002316d0b1dbfca79ca4d5cdd1547e6ba54b
wang_wenjie <wangwenjie@ewell.cc>
2018-07-11 08:48:13 +0800

    scrapy初始化链接简写

---
+++ simple_scrapy_project/spiders/simpleUrl_spider.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+    scrapy初始化url有两种方法
+    1,常量 start_urls; 并且必须定义一个方法parse()方法
+    2,直接定义一个方法: start_requests()
+"""
+
+import scrapy
+
+class simpleUrl(scrapy.Spider):
+
+
+    name = 'simpleUrl'
+
+    start_urls = ['http://lab.scrapyd.cn/page/1/',
+            'http://lab.scrapyd.cn/page/2/']
+
+    def parse(self, response):
+        page = response.url.split('/')[-2]
+        filename = 'simpleUrl_%s.html' % page
+
+        with open(filename, 'wb'):
+            filename.write(response.body)
+
+            self.log('保存文件 %s' % filename)
--- simple_scrapy_project/spiders/simple_spider.py
+++ simple_scrapy_project/spiders/simple_spider.py
@@ -3,18 +3,23 @@
 
 import scrapy
 
+# 1,创建一个类继承scrapy的子类
 class simple_spider(scrapy.Spider):
 
+    # 定义蜘蛛的名字
     name = 'simple_spider'
 
+    # 执行scrapy的方法 爬取链接网页
     def start_requests(self):
+
+        #定义需要爬取的网站
         urls = [
             'http://lab.scrapyd.cn/page/1/',
             'http://lab.scrapyd.cn/page/2/'
         ]
 
         for url in urls:
-            yield scrapy.Request(url=url, callback= self.parse)
+            yield scrapy.Request(url=url, callback=self.parse) #将爬取到的网页交给parse处理
 
 
     def parse(self, response):
@@ -23,6 +28,6 @@ class simple_spider(scrapy.Spider):
         filename = 'simple-%s.html' % page
 
         with open(filename, 'wb') as f:
-            f.write(response.body)
+            f.write(response.body)  #response.body 就是爬取的网页
 
             self.log('保存文件: %s' % filename)
